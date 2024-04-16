f4fb95bcd9bac7be9295cf2ee9a9b5fdc4ad4cfa
Angel Santiago Delgado Mendoza <angelsandmdz@gmail.com>
2024-04-16 00:37:21 -0500

    Se agregan cambios en js y css

---
+++ js/default.js
@@ -0,0 +1,3 @@
+const header = $('#_header');
+const mainBanner = $('home-banner-wrapper.main-banner');
+mainBanner.css('top', '-' + header.height() + 'px');
---
+++ js/sticky-header.js
@@ -0,0 +1,28 @@
+const header = $('#_header');
+let lastScrollTop = 0;
+
+$(window).on('scroll', function() {
+    let currentScrollTop = $(this).scrollTop(); // Obtenemos la nueva posición del scroll
+
+    if (currentScrollTop > lastScrollTop && currentScrollTop > 300){
+        header.addClass('header-scrolled');
+    } else {
+        if(currentScrollTop < 301) {
+            header.removeClass('header-scrolled');
+        }
+
+    }
+
+    header.on('mouseenter', function(){
+        $(this).addClass('header-scrolled');
+    });
+    
+    header.on('mouseleave', function(){
+        if(currentScrollTop < 301) {
+            $(this).removeClass('header-scrolled');
+        }
+    });
+    
+    lastScrollTop = currentScrollTop;
+});
+
